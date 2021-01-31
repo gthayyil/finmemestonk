d72ef5dbc2b1200159f9cbb90c5739c40f6f61a7
Georgy <gthayyil@tamu.edu>
2021-01-31 08:51:05 -0600

    Update views.py

--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template,url_for
 from FlaskWebProject1 import app
 from flask import request
 import numpy as np
@@ -161,7 +161,7 @@ def home():
         plt.xlabel("Days")
         plt.ylabel("Adj. Close Price $")
         plt.legend()
-        plt.savefig("FlaskWebProject1\\static\\images\\_graph.png")
+        plt.savefig(url_for("static")+"images/_graph.png")
 
         # plt.show()
         return render_template('index.html',
